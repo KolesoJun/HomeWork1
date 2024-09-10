e6d23461fbdba09b981b22bc7ab157613a610869
KolesoJun <danil187crr@yandex.ru>
2024-09-10 19:27:32 +0300

    11

---
+++ homework/BoxExpload1.0/ColorManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorManager : MonoBehaviour
+{
+    [SerializeField] private Color32[] _colors;
+
+    private void Awake()
+    {
+        gameObject.GetComponent<MeshRenderer>().material.color = ChangeRandom();
+    }
+
+    private Color32 ChangeRandom()
+    {
+        int randomMin = 0;
+        int randomMax = _colors.Length;
+        return _colors[Random.Range(randomMin, randomMax)];
+    }
+}
---
+++ homework/BoxExpload1.0/Fuze.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Fuze : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem _effect;
+    [SerializeField] private float _radius;
+    [SerializeField] private float _forceExpload;
+    [SerializeField] private Fuze _prefab;
+
+    private static float _chanceSeparation = 100;
+
+    private int _divisionThreshold = 50;
+    private int _indexSeparate = 2;
+    private int _sizeDivision = 2;
+
+    private void OnMouseUpAsButton()
+    {
+        Action();
+    }
+
+    private void Action()
+    {
+        Division();
+        Instantiate(_effect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+    private void Division()
+    {
+        if (_chanceSeparation >= _divisionThreshold)
+        {
+            int randomMin = 2;
+            int randomMax = 6;
+            int countNewObjects = Random.Range(randomMin, randomMax);
+
+            for (int i = 0; i < countNewObjects; i++)
+            {
+                Fuze prefab = Instantiate(_prefab, transform.position, Quaternion.identity);
+                prefab.GetComponent<Rigidbody>().AddExplosionForce(_forceExpload, transform.position, _radius);
+                prefab.transform.localScale /= _sizeDivision;
+                prefab.CalculateChanceSeparation();
+            }
+        }
+    }
+
+    public float CalculateChanceSeparation()
+    {
+        return _chanceSeparation /= _indexSeparate;
+    }
+}
